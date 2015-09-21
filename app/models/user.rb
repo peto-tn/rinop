1fcca014bc1deb30c2ff4a8a1b9d7444f4fae067
Ryo Jozaki <zaki0ry@gmail.com>
2015-09-21 15:44:33 +0900

    テキストはユーザが画像に対して投稿する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :images
+  has_many :texts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921054627) do
+ActiveRecord::Schema.define(version: 20150921063024) do
 
   create_table "images", force: true do |t|
     t.string   "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20150921054627) do
     t.integer  "favorite"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "image_id"
   end
 
   create_table "users", force: true do |t|
